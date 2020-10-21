19c3882e1da2f038c8e88a9de733bcca9b5ba858
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-21 16:41:45 +0200

    Offer

--- js/main.js
+++ js/main.js
@@ -15,7 +15,39 @@ var annulla = document.getElementById("annulla");
 
 // EVENTI
 
+// CALCOLO BIGLIETTO
 btnGenera.addEventListener('click', function() {
    var nome = document.getElementById("nome").value;
     console.log(nome);
+    var kmDapercorrere = document.getElementById("km").value;
+    console.log(kmDapercorrere);
+    var fasciaEta = document.getElementById("fascia-eta").value;
+    console.log(fasciaEta);
+
+    // CALCOLO COSTO
+    var prezzoKm = 0.21;
+    var costoBiglietto = prezzoKm * kmDapercorrere;
+    var offerta = 'Biglietto Standard';
+
+    // SCONTO 
+    if  ( fasciaEta == 'minorenne') {
+        costoBiglietto -= costoBiglietto * 0.2;
+        offerta = 'Offerta Minorenne';
+    } else if ( fasciaEta == 'over 65') {
+        costoBiglietto -= costoBiglietto * 0.4;
+        offerta = 'Offerta Over 65';
+
+
+        costoBiglietto = costoBiglietto.toFixed(2) + "€" 
+
+        // CARROZZA + NUMERO CP
+        var numeroCarrozza = Math.floor(Math.random() * 9) + 1; 
+       var numeroCp = Math.floor(Math.random() * (10000 - 9000) )+ 9000 ; 
+
+       document.getElementById('nome-passeggero').innerHTML = nome;
+       document.getElementById('offerta-applicata').innerHTML = offerta;
+       document.getElementById('carrozza').innerHTML = numeroCarrozza;
+       document.getElementById('codice-cp').innerHTML = numeroCp;
+       
 });
+
